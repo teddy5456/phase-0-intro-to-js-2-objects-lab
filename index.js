10bc0567a36763d90afcd8e5a6bff5060910f3a8
teddy wambua <teddywambua5456@gmail.com>
2023-03-14 02:17:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: 'value',
+}
+ // updateEmployeeWithKeyAndValue function
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  }
+  
+  // destructivelyUpdateEmployeeWithKeyAndValue function
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // deleteFromEmployeeByKey function
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // destructivelyDeleteFromEmployeeByKey function
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
